dc0617f823b6b12af3635931400be4a5f6ae5d24
Griffin Cleverly <gtcleverly@gmail.com>
2016-11-19 17:33:54 -0800

    added/modified comments

--- More_API_practice.py
+++ More_API_practice.py
@@ -4,10 +4,9 @@
 headers = {"Authorization": "token 1f36137fbbe1602f779300dad26e4c1b7fbab631"}
 
 # Make a GET request to the Github API with our headers.
-# This API endpoint will give us details about Vik Paruchuri.
 response = requests.get("https://api.github.com/users/VikParuchuri", headers=headers)
 
-# Print the content of the response.  As you can see, this token is associated with the account of Vik Paruchuri.
+# Print the content of the response
 print(response.json())
 
 response_2 = requests.get("https://api.github.com/users/VikParuchuri/orgs", headers=headers)
@@ -15,14 +14,12 @@ orgs = response_2.json()
 
 ## 3. Endpoints and objects ##
 
-# headers is loaded in.
 response = requests.get("https://api.github.com/users/torvalds", headers=headers)
 torvalds = response.json()
 
 
 ## 4. Other objects ##
 
-# Enter your answer here.
 response = requests.get("https://api.github.com/repos/octocat/Hello-World", headers=headers)
 hello_world = response.json()
 
@@ -38,16 +35,12 @@ page2_repos = response_2.json()
 
 ## 6. User-level endpoints ##
 
-# Enter your code here.
 response = requests.get("https://api.github.com/user", headers=headers)
 user = response.json()
 
 ## 7. POST requests ##
 
-# Create the data we'll pass into the API endpoint.  This endpoint only requires the "name" key, but others are optional.
 payload = {"name": "test"}
-
-# We need to pass in our authentication headers!
 response = requests.post("https://api.github.com/user/repos", json=payload, headers=headers)
 print(response.status_code)
 
