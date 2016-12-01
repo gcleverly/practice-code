05132b745e21c205fc476456b9a2d571a2f8830e
Griffin Cleverly <gtcleverly@gmail.com>
2016-11-30 22:35:05 -0800

    nw comments

--- wordnikapi.py
+++ wordnikapi.py
@@ -1,4 +1,5 @@
 # This short code makes requests to wordniks API to get definitions
+
 import requests
 
 WORDNIK_URL = 'http://api.wordnik.com:80/v4/word.json/article/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
